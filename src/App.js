70e3a472985acd8e30237d38c3604ec5d368f797
Ludovic Parriaud <wtfshenei@gmail.com>
2023-07-08 08:35:05 +0200

    feat(projet): structure + routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Housing from "./pages/Housing/Housing";
+import Error404 from "./pages/Error404/Error404";
 
 const App = () => {
   return (
-    <div>
-      <h1>OCR Projet 8</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/housing" element={<Housing />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/Housing/Housing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Housing = () => {
+    return (
+        <div>
+            <h1>HOUSING</h1>
+        </div>
+    );
+};
+
+export default Housing;

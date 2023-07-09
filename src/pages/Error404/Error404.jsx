a180f0e6cab679d10becee263e5633bf3f04b672
Ludovic Parriaud <wtfshenei@gmail.com>
2023-07-09 12:33:22 +0200

    fix(erreur404): design de la page

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import Home from '../Home/Home';
+import { Link } from 'react-router-dom';
 
 const Error404 = () => {
     return (
-        <div>
+        <div className='error-container'>
             <Header />
-            <h1>ERROR 404</h1>
+            <span className='error'>404</span>
+            <p className='error-p'>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to={'/'}>
+                <button>Retourner sur la page d'accueil</button>
+            </Link>
+            <Footer />
         </div>
     );
 };

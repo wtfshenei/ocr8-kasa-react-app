2803d073628c8305a8d74c7d74c4cc1fd1c49318
Ludovic Parriaud <wtfshenei@gmail.com>
2023-07-13 14:12:34 +0200

    feat(mobile): ajout des media queries

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
-    return (
-        <div className='error-container'>
-            <Header />
-            <span className='error'>404</span>
-            <p className='error-p'>Oups! La page que vous demandez n'existe pas.</p>
-            <Link to={'/'}>
-                <button>Retourner sur la page d'accueil</button>
-            </Link>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <div className="error-container">
+        <span className="error">404</span>
+        <p className="error-p">Oups! La page que vous demandez n'existe pas.</p>
+        <Link to={"/"}>
+          <button>Retourner sur la page d'accueil</button>
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Error404;
